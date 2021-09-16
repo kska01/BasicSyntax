500c70585338e1dcc80cb26d2e2b212cbec6ad0d
ka <insideka01@gmail.com>
2021-09-16 15:00:19 +0900

    두번째 dice 완성본

--- DiceRoller2/app/src/main/java/com/example/diceroller2/MainActivity.kt
+++ DiceRoller2/app/src/main/java/com/example/diceroller2/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller2
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Toast message
 //        val rollButton: Button = findViewById(R.id.button)
 //        rollButton.setOnClickListener {
 //            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
@@ -20,19 +21,34 @@ class MainActivity : AppCompatActivity() {
 //        }
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        // Do a dice roll when the app starts
+        rollDice()
     }
 
     /**
-     * Rloo the dice and update the screen with the result.
+     * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it.
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        // Update the screen with the dice roll.
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        // ImageView 코드소스와 연결하기. Determine which drawable resource ID to use based on the dice roll
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when(diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
