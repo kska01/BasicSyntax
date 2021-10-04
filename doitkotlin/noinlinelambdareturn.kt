0c7fd67ababf47ab6c855db54db953ce7f25d259
ka <insideka01@gmail.com>
2021-10-04 17:54:07 +0900

    라벨을 사용한 람다식의 반환

---
+++ doitkotlin/noinlinelambdareturn.kt
@@ -0,0 +1,19 @@
+package chap04.secion3.noinline
+
+fun main() {
+    retFun()
+}
+
+fun inlineLambda(a: Int, b: Int, out: (Int, Int) -> Unit) {
+    out(a, b)
+}
+
+fun retFun() {
+    println("start of retFunc")
+    inlineLambda(13, 3) lit@{ a, b -> 
+        val result = a + b
+        if(result > 10) return@lit 
+        println("result: $result")
+    }
+    println("end of retFunc")
+}
