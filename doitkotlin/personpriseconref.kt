687a7e7b4ec253721eccc87a57f467af255372eb
ka <insideka01@gmail.com>
2021-10-11 11:35:46 +0900

    주 생성자와 부 생성자 함께 사용하기

--- doitkotlin/personpriseconref.kt
+++ doitkotlin/personpriseconref.kt
@@ -1,11 +1,11 @@
 package chap05.section4.prisecon
 
 class Person(firstName: String, out: Unit = println("[Primary Constructor] Parameter")) {
+    val fName = println("[Property] Person fName: $firstName")
     
     init {
         println("[init] Person init block")
     }
-    val fName = println("[Property] Person fName: $firstName")
 
     constructor(firstName: String, age: Int, 
             out: Unit = println("[Secondary Constructor] Parameter")) : this(firstName) {
