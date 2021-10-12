ddc18dbd17841f12c24456ca5bc2e8bb9ec8751c
ka <insideka01@gmail.com>
2021-10-13 07:50:35 +0900

    재귀함수

---
+++ kkangkotlin/ch4tailrec1.kt
@@ -0,0 +1,10 @@
+package com.example.ch4.two 
+
+fun recPrint(no: Int = 1, count: Int = 1) {
+    println("recPrint...")
+    return if(no === count) return else recPrint(no - 1, count)
+}
+
+fun main() {
+    recPrint(3)
+}
