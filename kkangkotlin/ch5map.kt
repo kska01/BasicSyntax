3026b76d39d098cf101072c05b28a15f39505d26
ka <insideka01@gmail.com>
2021-10-13 15:19:02 +0900

    map , mutableMap

---
+++ kkangkotlin/ch5map.kt
@@ -0,0 +1,13 @@
+package five_two_two
+
+fun main() {
+    val immutableMap1 = mapOf<String, String>(Pair("one", "hello"), Pair("two", "world"))
+    println("${immutableMap1.get("one")} .. ${immutableMap1.get("two")}")
+    val immutableMap2 = mapOf<String, String>("one" to "hello", "two" to "kkang")
+    println("${immutableMap2.get("one")} .. ${immutableMap2.get("two")}")
+    
+    val mutableMap = mutableMapOf<String, String>()
+    mutableMap.put("one","hello")
+    mutableMap.put("two", "map")
+    println("${mutableMap.get("one")} .. ${mutableMap.get("two")}")
+}
