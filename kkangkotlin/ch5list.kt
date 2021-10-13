ecdda67973863a6cf41a69e041ea3baacb10d176
ka <insideka01@gmail.com>
2021-10-13 14:55:50 +0900

    List, MutableList

---
+++ kkangkotlin/ch5list.kt
@@ -0,0 +1,11 @@
+package five_two_two
+
+fun main() {
+    val immutableList: List<String> = listOf("hello", "world")
+    println("${immutableList.get(0)} .. ${immutableList.get(1)}")
+
+    val mutableList: MutableList<String> = mutableListOf("hello", "world")
+    mutableList.add("kkang")
+    mutableList.set(1, "korea")
+    println("${mutableList.get(0)} .. ${mutableList.get(1)} .. ${mutableList.get(2)}")
+}
