073f2a9e6ce555d7571fb08686f1929b68e51dea
ka <insideka01@gmail.com>
2021-10-13 15:01:22 +0900

    List, MutableList, ArrayList

--- kkangkotlin/ch5list.kt
+++ kkangkotlin/ch5list.kt
@@ -8,4 +8,10 @@ fun main() {
     mutableList.add("kkang")
     mutableList.set(1, "korea")
     println("${mutableList.get(0)} .. ${mutableList.get(1)} .. ${mutableList.get(2)}")
+
+    val arrayList: ArrayList<String> = ArrayList()
+    arrayList.add("hello")
+    arrayList.add("kkang")
+    arrayList.set(1, "world")
+    println("${arrayList.get(0)} .. ${arrayList.get(1)}")
 }
