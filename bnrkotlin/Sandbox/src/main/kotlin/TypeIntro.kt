1d8514b383d5981755f61498eba4f6daeb03b6f7
ka <insideka01@gmail.com>
2021-10-16 21:31:46 +0900

    ch2

---
+++ bnrkotlin/Sandbox/src/main/kotlin/TypeIntro.kt
@@ -0,0 +1,15 @@
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main() {
+    val playerName = "에스트라곤"
+    var experiencePoints = 5
+    var hasSteed = false
+    val nameOfBar = "Unicorn's Horn"
+    val nameOfBarOwner = "Owner"
+    var goldOfPlayer = 50
+    var reversedName = playerName.reversed()
+    println(reversedName)
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+}
