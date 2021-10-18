40d1f2bdd4385b5b9c657628ab52652e9a6781bf
ka <insideka01@gmail.com>
2021-10-18 23:52:57 +0900

    10.18 ch4 함수

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -12,6 +12,10 @@ fun main() {
     // 플레이어의 상태 출력
     printPlayerStatus(auraColor, isBlessed, name, healthStatus)
     castFireball()
+
+    performCombat()
+    performCombat("Ulrich")
+    performCombat("Hildr", true)
 }
 
 private fun printPlayerStatus(
@@ -45,3 +49,19 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
 
 private fun castFireball(numFireballs: Int = 2) =
     println("한 덩어리의 파이어볼이 나타난다. (x$numFireballs)")
+
+fun performCombat() {
+    println("적군이 없다!")
+}
+
+fun performCombat(enemyName: String) {
+    println("$enemyName 과 전투를 시작함.")
+}
+
+fun performCombat(enemyName: String, isBlessed: Boolean) {
+    if (isBlessed) {
+        println("$enemyName 과 전투를 시작함. 축복을 받음!")
+    } else {
+        println("$enemyName 과 전투를 시작함.")
+    }
+}
