95420e9108e5a44045eb1d84b6f246edb44feff4
ka <insideka01@gmail.com>
2021-10-18 23:21:48 +0900

    10.18 ch4 함수

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -5,23 +5,43 @@ fun main() {
     val isImmortal = false
 
     // 아우라
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
 
-    val healthStatus = when (healthPoints) {
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    // 플레이어의 상태 출력
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+    castFireball()
+}
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println(
+        "(Aura: $auraColor) " +
+                "(Blessed: ${if (isBlessed) "YES" else "NO"})"
+    )
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean) =
+    if (isBlessed && healthPoints > 50 || isImmortal) "GREEN" else "NONE"
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+    when (healthPoints) {
         100 -> "최상의 상태임!"
         in 90..99 -> "약간의 찰과상만 있음."
         in 75..89 -> if (isBlessed) {
-                "경미한 상처가 있지만 빨리 치유되고 있음!"
-            } else {
-                "경미한 상처만 있음."
-            }
+            "경미한 상처가 있지만 빨리 치유되고 있음!"
+        } else {
+            "경미한 상처만 있음."
+        }
         in 15..74 -> "많이 다친 것 같음."
         else -> "최악의 상태임!"
     }
 
-    // 플레이어의 상태 출력
-    println("(Aura: $auraColor) " +
-            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
-}
+private fun castFireball(numFireballs: Int = 2) =
+    println("한 덩어리의 파이어볼이 나타난다. (x$numFireballs)")
