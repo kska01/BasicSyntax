2066bf7e8122f77362a988647b12098bbf219ba8
ka <insideka01@gmail.com>
2021-10-19 17:47:35 +0900

    ch5 익명 함수와 함수 타입 - 반환 타입으로 함수 타입 사용하기

--- Sandbox/src/main/kotlin/SimVillage.kt
+++ Sandbox/src/main/kotlin/SimVillage.kt
@@ -1,20 +1,19 @@
 fun main() {
-    runSimulation("김선달", ::printConstructionCost) { playerName, numBuildings ->
+    runSimulation()
+}
+
+fun runSimulation() {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("김선달"))
+}
+
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "병원"
+    var numBuildings = 5
+    return { playerName: String ->
         val currentYear = 2019
-        println("$numBuildings 채의 건물이 추가됨")
-        "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
+        numBuildings += 1
+        println("$numBuildings 채의 $structureType 이 추가됨")
+        "SimVillage 방문을 환영합니다, $playerName! (copyright $currentYear)"
     }
-}
-
-inline fun runSimulation(playerName: String,
-                         costPrinter: (Int) -> Unit,
-                         greetingFunction: (String, Int) -> String) {
-    val numBuildings = (1..3).shuffled().last()   // 1, 2, 3 중 하나를 무작위로 선택한다
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName, numBuildings))
-}
-
-fun printConstructionCost(numBuildings: Int) {
-    val cost = 500
-    println("건축 비용: ${cost * numBuildings}")
 }
