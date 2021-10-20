8fefd7af96574c6ffd47da36f92133af00dd071c
ka <insideka01@gmail.com>
2021-10-20 10:14:39 +0900

    10.20 ch6 null 안전과 예외 - 값이 null인지 if로 검사하기 - null 복합 연산자

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -1,12 +1,19 @@
 fun main() {
-    var beverage = readLine()?.let {
-        if (it.isNotBlank()) {
-            it.capitalize()
-        } else {
-            "맥주"
-        }
-    }
+    var beverage = readLine()
 //    beverage = null
 
-    println(beverage)
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("beverage가 null입니다!")
+    }
+
+// 위의 if else 구문과 같은 내용의 코드
+//    var beverage = readLine()
+//    beverage?.let {
+//        beverage = it.capitalize()
+//    } ?: println("beverage가 null입니다.!")
+
+    val beverageServed: String = beverage ?: "맥주"
+    println(beverageServed)
 }
