fed4f78d6dbd74320a7fd261f9cb65539a6f88e7
ka <insideka01@gmail.com>
2021-10-20 10:17:12 +0900

    10.20 ch6 null 안전과 예외 - 값이 null인지 if로 검사하기 - null 복합 연산자

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -8,7 +8,7 @@ fun main() {
         println("beverage가 null입니다!")
     }
 
-// 위의 if else 구문과 같은 내용의 코드
+// 안전 호출 연산자+let+null 복합 연사자를 사용한 위의 if else 구문과 같은 내용의 코드
 //    var beverage = readLine()
 //    beverage?.let {
 //        beverage = it.capitalize()
