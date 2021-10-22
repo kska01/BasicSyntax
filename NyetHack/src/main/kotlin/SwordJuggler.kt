700f46fc1e94e080ff84413b7cf8972d05c2924d
ka <insideka01@gmail.com>
2021-10-23 00:09:10 +0900

    10.20 ch6 null 안전과 예외 - 예외 - IllegalStateException던지기

--- NyetHack/src/main/kotlin/SwordJuggler.kt
+++ NyetHack/src/main/kotlin/SwordJuggler.kt
@@ -5,7 +5,12 @@ fun main() {
         swordsJuggling =2
     }
 
+    proficiencyCheck(swordsJuggling)
     swordsJuggling = swordsJuggling!!.plus(1)
 
     println("$swordsJuggling 개의 칼로 저글링합니다!")
+}
+
+fun proficiencyCheck(swordsJuggling: Int) {
+    swordsJuggling ?: throw IllegalStateException("플레이어가 저글링을 할 수 없음")
 }
