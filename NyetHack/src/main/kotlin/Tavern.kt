685865611b0b8c667fd63999b6c3e3f35140d986
ka <insideka01@gmail.com>
2021-10-27 16:58:34 +0900

    10.27 ch7 문자열 - 문자열 변경하기 - 7.5 toDragonSpeak 함수 추가하기 - replace()

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -1,7 +1,20 @@
 const val TAVERN_NAME = "Taernyl's Folly"
+
 fun main() {
     placeOrder("shandy,Dragon's Breath,5.91")
 }
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
 
 private fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
@@ -11,4 +24,7 @@ private fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.split(',')
     val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
     println(message)
+
+    val phrase = "와, $name 진짜 좋구나!"
+    println("마드리갈이 감탄한다: ${toDragonSpeak(phrase)}")
 }
