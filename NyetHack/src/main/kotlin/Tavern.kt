67e7965eb38ec055844d4340371d1c90969f78cd
ka <insideka01@gmail.com>
2021-10-27 16:03:14 +0900

    10.27 ch7 문자열 - 7.1 술집 주인의 이름 추출하기 - indexOf(), substring()

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -1,19 +1,10 @@
+const val TAVERN_NAME = "Taernyl's Folly"
 fun main() {
-    var beverage = readLine()
-//    beverage = null
+    placeOrder()
+}
 
-    if (beverage != null) {
-        beverage = beverage.capitalize()
-    } else {
-        println("beverage가 null입니다!")
-    }
-
-// 안전 호출 연산자+let+null 복합 연사자를 사용한 위의 if else 구문과 같은 내용의 코드
-//    var beverage = readLine()
-//    beverage?.let {
-//        beverage = it.capitalize()
-//    } ?: println("beverage가 null입니다.!")
-
-    val beverageServed: String = beverage ?: "맥주"
-    println(beverageServed)
+private fun placeOrder() {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("마드리갈은 $tavernMaster 에게 주문한다.")
 }
