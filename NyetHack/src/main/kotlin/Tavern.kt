e402eb7a0bc6c47884f8aeaffc0ea700a12e7bbf
ka <insideka01@gmail.com>
2021-10-27 16:29:49 +0900

    10.27 ch7 문자열 - 7.4 주문 데이터의 해체 선언  - split()

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -8,10 +8,7 @@ private fun placeOrder(menuData: String) {
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("마드리갈은 $tavernMaster 에게 주문한다.")
 
-    val data = menuData.split(',')
-    val type = data[0]
-    val name = data[1]
-    val price = data[2]
+    val (type, name, price) = menuData.split(',')
     val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
     println(message)
 }
