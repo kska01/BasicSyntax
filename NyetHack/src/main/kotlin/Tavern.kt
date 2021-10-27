351e424e12864286b2d94ceb5090f3f2ea820317
ka <insideka01@gmail.com>
2021-10-27 16:24:55 +0900

    10.27 ch7 문자열 - 7.3 메뉴 데이터 분리하기 - split()

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -1,10 +1,17 @@
 const val TAVERN_NAME = "Taernyl's Folly"
 fun main() {
-    placeOrder()
+    placeOrder("shandy,Dragon's Breath,5.91")
 }
 
-private fun placeOrder() {
+private fun placeOrder(menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("마드리갈은 $tavernMaster 에게 주문한다.")
+
+    val data = menuData.split(',')
+    val type = data[0]
+    val name = data[1]
+    val price = data[2]
+    val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
+    println(message)
 }
