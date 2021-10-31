0081340559210a76694f6c3fe1ef9f668c06b67d
ka <insideka01@gmail.com>
2021-10-31 23:54:38 +0900

    10.31 ch8 숫자 - 정수 - 8.1 플레이어의 지갑 설정하기

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -1,8 +1,18 @@
 const val TAVERN_NAME = "Taernyl's Folly"
 
+var playerGold = 10
+var playerSilver = 10
+
 fun main() {
-    //placeOrder("shandy,Dragon's Breath,5.91")
-    placeOrder("elixir,Shirley's Temple,4.12")
+    placeOrder("shandy,Dragon's Breath,5.91")
+}
+
+fun performPurchase() {
+    displayBalance()
+}
+
+private fun displayBalance() {
+    println("플레이어의 지갑 잔액: 금화: $playerGold 개, 은화: $playerSilver 개")
 }
 
 private fun toDragonSpeak(phrase: String) =
@@ -30,6 +40,8 @@ private fun placeOrder(menuData: String) {
     val (type, name, price) = menuData.split(',')
     val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
     println(message)
+
+    performPurchase()
 
     val phrase = if (name == "Dragon's Breath") {
         "마드리갈이 감탄한다: ${toDragonSpeak("와, $name 진짜 좋구나!")}"
