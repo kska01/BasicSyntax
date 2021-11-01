2ebcf1ef125e467321fdc3f8712cc118a869cd29
ka <insideka01@gmail.com>
2021-11-01 23:59:49 +0900

    11.1 ch10 List와 Set - List - 10.1 고객 이름 List 생성하기

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -3,9 +3,12 @@ const val TAVERN_NAME = "Taernyl's Folly"
 
 var playerGold = 10
 var playerSilver = 10
+val patronList: List<String> = listOf("Eli", "Mordoc", "Sophie")
 
 fun main() {
     placeOrder("shandy,Dragon's Breath,5.91")
+
+    println(patronList)
 }
 
 fun performPurchase(price: Double) {
