ad78f8834e43cd6e5df22a92d8b837788d1b550d
ka <insideka01@gmail.com>
2021-11-05 07:59:50 +0900

    11.5 백준 10869번

---
+++ baekjoon/10869.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+    println(a % b)
+}
