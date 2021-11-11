0835235a8cd7cf5fb668b7b9ab33210204d7ee58
ka <insideka01@gmail.com>
2021-11-11 21:29:15 +0900

    11.11 ch12 클래스 정의하기 - 클래스 함수 - 12.4 Player 클래스 함수 호출하기

--- NyetHack/src/main/kotlin/Game.kt
+++ NyetHack/src/main/kotlin/Game.kt
@@ -5,6 +5,7 @@ fun main() {
     val isImmortal = false
 
     val player = Player()
+    player.castFireball()
 
     // 아우라
     val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
@@ -13,7 +14,6 @@ fun main() {
 
     // 플레이어의 상태 출력
     printPlayerStatus(auraColor, isBlessed, name, healthStatus)
-    castFireball()
 
     performCombat()
     performCombat("Ulrich")
@@ -49,8 +49,6 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
         else -> "최악의 상태임!"
     }
 
-private fun castFireball(numFireballs: Int = 2) =
-    println("한 덩어리의 파이어볼이 나타난다. (x$numFireballs)")
 
 fun performCombat() {
     println("적군이 없다!")
