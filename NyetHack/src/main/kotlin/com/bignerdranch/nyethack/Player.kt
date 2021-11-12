787844d1047a1d9816d7d24664841b7129f5295e
ka <insideka01@gmail.com>
2021-11-12 14:49:48 +0900

    11.12 ch13 초기화 - 기본 생성자에 속성 정의하기 - 13.3 기본 생성자에 속성 정의하기

--- NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Player.kt
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Player.kt
@@ -1,19 +1,15 @@
 package com.bignerdranch.nyethack
 
 class Player(_name: String,
-             _healthPoints: Int,
-             _isBlessed: Boolean,
-             _isImmortal: Boolean) {
+             var healthPoints: Int,
+             val isBlessed: Boolean,
+             private val isImmortal: Boolean) {
 
     var name = _name
         get() = field.capitalize()
         private set(value) {
             field = value.trim()
         }
-
-    var healthPoints = _healthPoints
-    val isBlessed = _isBlessed
-    private val isImmortal = _isImmortal
 
     fun auraColor() =
         if (isBlessed && healthPoints > 50 || isImmortal) "GREEN" else "NONE"
