2587d8f5a63fb44789eb5598f5e10042c3891f85
ka <insideka01@gmail.com>
2021-11-17 16:31:11 +0900

    11.17 ch15 객체 - 연산자 오버로딩 - 15.16 ~ 15.17 plus 함수 오버로딩하고 연산자 사용하기

--- NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
+++ NyetHack/src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
@@ -7,9 +7,11 @@ enum class Direction(private val coordinate: Coordinate) {
     WEST(Coordinate(-1,0));
 
     fun updateCoordinate(playerCoordinate: Coordinate) =
-        Coordinate(playerCoordinate.x + coordinate.x, playerCoordinate.y + coordinate.y)
+        Coordinate + playerCoordinate
 }
 
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
+
+    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
 }
