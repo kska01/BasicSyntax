7edbbbbf1096960ae993f4cbaab0af981ca52126
ka <insideka01@gmail.com>
2021-11-18 08:43:22 +0900

    11.18 A + B - 3

---
+++ baekjoon/10950.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+
+    for (i in 1..n) {
+        val a = sc.nextInt()
+        val b = sc.nextInt()
+        println(a + b)
+    }
+}
