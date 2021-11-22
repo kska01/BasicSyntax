f3617fe0ee22930338177febd5946af7fdca17f9
ka <insideka01@gmail.com>
2021-11-22 16:14:12 +0900

    같은 이름의 추상 함수 여러 개일 때

--- kkangkotlin/ij/kkang/src/main/kotlin/ten_two_three/ch10abstracs.kt
+++ kkangkotlin/ij/kkang/src/main/kotlin/ten_two_three/ch10abstracs.kt
@@ -14,11 +14,7 @@ open abstract class Super2 {
 
 class Sub2: Super2(), Interface1, Interface2 {
     override fun funA() {
-<<<<<<< HEAD
         println("Sub2 funA...")
-=======
-        println("Sub2 funA...")
->>>>>>> 379d7ed4a1420e6dfebb5d96eecdae0c6ac32315
     }
 }
 
