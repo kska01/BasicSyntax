ec7a8e6f4510bec19725866401c93e493e4df011
ka <insideka01@gmail.com>
2021-11-23 00:19:47 +0900

    object로 클래스 작성

---
+++ kkangkotlin/ch11object.kt
@@ -0,0 +1,17 @@
+package eleven_five_one
+
+val obj1 = object {
+    var no1: Int = 10
+    fun myFun() {
+
+    }
+}
+
+class Outer {
+    val obj2 = object {
+        var no2: Int = 0
+        fun myFun() {
+            
+        }
+    }
+}
