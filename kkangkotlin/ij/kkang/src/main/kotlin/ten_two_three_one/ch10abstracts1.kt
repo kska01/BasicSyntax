08f99b0f8fb5c337f767ddaa70d5ea90c4126d88
ka <insideka01@gmail.com>
2021-11-22 15:41:25 +0900

    인리제이를 이용한 kkangkotlin

---
+++ kkangkotlin/ij/kkang/src/main/kotlin/ten_two_three_one/ch10abstracts1.kt
@@ -0,0 +1,27 @@
+package ten_two_three_one
+
+interface Interface3 {
+    fun funA() {
+        println("Interface3 funA....")
+    }
+}
+
+open abstract class Super3 {
+    abstract fun funA()
+}
+
+class Sub3: Super3(), Interface3 {
+    override fun funA() {
+        super.funA()
+        println("Sub3 funA....")
+    }
+    fun some() {
+        super.funA()
+    }
+}
+
+fun main() {
+    val obj1 = Sub3()
+    obj1.funA()
+    obj1.some()
+}
