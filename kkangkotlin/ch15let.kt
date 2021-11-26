988eb3a3d44cc2d89ef76bf9de7de1e26569e9e6
ka <insideka01@gmail.com>
2021-11-26 12:56:29 +0900

    lt{ }을 이용한 구문 실행

---
+++ kkangkotlin/ch15let.kt
@@ -0,0 +1,16 @@
+fun main () {
+
+    val array = arrayOf("hello", null, "kkang")
+
+    array.forEach {
+        if (it != null) {
+            println("$it .. ${it.length}")
+        }
+    }
+
+    array.forEach {
+        it?.let {
+            println("$it .. ${it.length}")
+        }
+    }
+}
