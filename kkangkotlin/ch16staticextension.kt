7abab1d4042f1b1386bca8fac368dad9b0d94794
ka <insideka01@gmail.com>
2021-11-27 08:15:02 +0900

    확장에 의한 구현 - 다형성, 정적 등록

---
+++ kkangkotlin/ch16staticextension.kt
@@ -0,0 +1,20 @@
+package sixteen_one_two_two
+
+open class Super
+
+class Sub : Super()
+
+fun Super.sayHello() {
+    println("Super..sayHello()")
+}
+fun Sub.sayHello() {
+    println("Sub..sayHello()")
+}
+
+fun some(obj: Super) {
+    obj.sayHello()
+}
+
+fun main() {
+    some(Sub())
+}
