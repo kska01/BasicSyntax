da73b57d144d1d74c279394096142255866968e7
ka <insideka01@gmail.com>
2021-11-26 13:47:10 +0900

    ?: 연산자

---
+++ kkangkotlin/ch15elvis.kt
@@ -0,0 +1,17 @@
+fun main () {
+    var data: String? = "kkang" 
+     
+    var length: Int = if(data != null) {
+        data.length
+    }else {
+        -1
+    }
+     
+    data = null
+     
+    length = data?.length ?: -1
+     
+    println(length)
+     
+    data ?: println("data is null")
+ }
