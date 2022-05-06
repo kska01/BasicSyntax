c4b896a15c0d1175a454ee5dac06495cfc3b5844
ka <insideka01@gmail.com>
2022-05-06 10:39:50 +0900

    05.05 별 찍기 - 1

---
+++ Baekjoon22/src/main/kotlin/2438-1.kt
@@ -0,0 +1,13 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    with(System.out.bufferedWriter()) {
+        val n = readLine().toInt()
+        repeat(n) {
+            repeat(it + 1) {
+                write("*")
+            }
+            write("\n")
+        }
+        flush()
+        close()
+    }
+}
